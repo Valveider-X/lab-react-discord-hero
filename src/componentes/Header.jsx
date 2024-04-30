5b1537c4e2e09d8ac52863eabeb39da24ac7cdcd
Xavi <valcreativo@gmail.com>
2024-04-30 13:28:44 +0200

    Solved

---
+++ src/componentes/Header.jsx
@@ -0,0 +1,25 @@
+import imgLogo from "../assets/images/logo.png"
+import imgBarrita from "../assets/images/barrita.png"
+function Header(){
+    return(
+<header>
+    <div id="manoloSpace">
+<img src={imgLogo} height="20px"></img>
+<img src={imgBarrita} height={"20px"}></img>
+</div>
+<br/>
+<h1>
+    IMAGINE A 
+</h1>
+<h1>
+    PLACE...
+    </h1>
+    <p>holi holita holi holita holi holita holi holita holi holita holi holita holi holita holi holita holi holita i holita holi holita holi holita holi holita holi holita holi holita holi holita holi holita holi holitai holita holi holita holi holita holi holita holi holita holi holita holi holita holi holita holi holita</p>
+   
+    <button id="button1">Download for Mac</button>
+    <br/>
+    <button id="button2">Open Discord in Your Bowser(its me mario)</button>
+</header>
+    )
+}
+export default Header
